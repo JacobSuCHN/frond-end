18815d75114a05fcce99bc38cc2c0f0d2963b215
JacobSu <suxinkevin51@163.com>
2025-01-05 19:25:43 +0800

    DAY15

--- 4.工程化/code/react-webpack-starter/src/main.js
+++
@@ -1,8 +0,0 @@
-import { App } from "./App";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./yk/index.yk";
-
-const app = ReactDOM.createRoot(document.getElementById("root"));
-
-app.render(<App />);
--- 4.工程化/code/webpack-starter/src/footer.js
+++
@@ -1,3 +0,0 @@
-export const getFooterDemo = () => {
-  console.log("Footer Demo");
-};
